fe15d6f90fa4349e3995eae79b15a26d0eeefe98
Miguel Da Silva <miguel@dasilvaf.net>
2025-05-08 21:50:39 +0200

    db migration that add contracts table

---
+++ sql/schema/002_contracts.sql
@@ -0,0 +1,36 @@
+-- +goose Up
+CREATE TYPE payments_t AS (
+  on_accepted INTEGER,
+  on_fulfilled INTEGER
+);
+
+CREATE TYPE deliver_t AS (
+  trade_symbol TEXT,
+  destination_symbol TEXT,
+  units_required INTEGER,
+  units_fulfilled INTEGER
+);
+
+CREATE TYPE terms_t AS (
+  deadline TIMESTAMP,
+  payments payments_t,
+  deliver deliver_t
+);
+
+CREATE TABLE contracts (
+  id TEXT NOT NULL PRIMARY KEY,
+  factionSymbol TEXT NOT NULL,
+  type TEXT NOT NULL,
+  terms terms_t NOT NULL,
+  accepted BOOLEAN NOT NULL DEFAULT false,
+  fulfilled BOOLEAN NOT NULL DEFAULT false,
+  deadline_to_accept TIMESTAMP NOT NULL
+);
+--
+
+-- +goose Down
+DROP TABLE contracts;
+DROP TYPE terms_t;
+DROP TYPE deliver_t;
+DROP TYPE payments_t;
+--
